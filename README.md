3e0dbcf2a3e4aeccd875fa4563c075c9abb082a1
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-11-12 10:33:32 -0300

    primerio component

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Mini curso do Canal Sujeito programador
 
 
 React js do zero - Aula 01 Iniciando sua jornada! - https://www.youtube.com/watch?v=K-njDh5pfLo&t=283s
+
+10:43
--- devlink/src/App.js
+++ devlink/src/App.js
@@ -2,6 +2,17 @@ export default function App(){
   return(
     <div>
       <h1>Meu primeiro projeto</h1>
+      <br/>
+      <Aluno/>
+    </div>
+  )
+}
+
+
+function Aluno(){
+  return(
+    <div>
+      <h1>Bem vindo ALuno</h1>
     </div>
   )
 }
