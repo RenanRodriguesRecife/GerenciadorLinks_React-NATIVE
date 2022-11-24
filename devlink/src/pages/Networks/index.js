bee84434df7d74de024d65cc6eff2c15e96cc21a
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-11-24 00:27:44 -0300

    social page

--- devlink/src/pages/Networks/index.js
+++ devlink/src/pages/Networks/index.js
@@ -46,4 +46,43 @@ export default function Networks(){
             console.log("Erro ao Salvar: " + error);
         })
     }
+
+    return(
+        <div className="admin-container">
+            <Header/>
+            <Logo/>
+            <h1 className="title-social">Suas redes sociais</h1>
+
+            <form className="form" onSubmit={handleSave}>
+                <label className="label">Link do Facebook</label>
+                <Input
+                    placeholder="Digite a url do Facebook"
+                    value={facebook}
+                    onChange={(e)=>setFacebook(e.target.value)}
+                    />
+                <label className="label">Link do Instagram</label>
+                <Input
+                    placeholder="Digite a url do Instagram"
+                    value={instagram}
+                    onChange={(e)=>setInstagram(e.target.value)}
+                />
+                <label className="label">Link do LinkedIn</label>
+                <Input
+                    placeholder="Digite a url do LinkedIn"
+                    value={linkedIn}
+                    onChange={(e)=>setLinkedIn(e.target.value)}
+                />
+
+                <label className="label">Link do Github</label>
+                <Input
+                    placeholder="Digite a url do Github"
+                    value={github}
+                    onChange={(e)=>setGithub(e.target.value)}
+                />
+                <button type="submit" className="btn-register">
+                    Salvar Links <MdAddLink size={24} color="#FFF"/>
+                </button>
+            </form>
+        </div>
+    )
 }
