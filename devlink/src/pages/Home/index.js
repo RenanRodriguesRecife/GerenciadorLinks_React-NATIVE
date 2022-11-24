4bf526ed6bde7c8737f8a5dd176705c8c9a819a1
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-11-24 07:09:44 -0300

    home update

--- devlink/src/pages/Home/index.js
+++ devlink/src/pages/Home/index.js
@@ -1,8 +1,62 @@
 import './home.css'
+import React, { useEffect, useState } from "react";
 import { Social } from '../../components/Social'
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
 
+import {
+    getDocs,
+    collection,
+    orderBy,
+    query,
+    doc,
+    getDoc,
+} from "firebase/firestore";
+
+import { db } from "../../services/firebaseConection";
+
 export default function Home(){
+    const [links,setLinks] = useState([]);
+    const [socialLinks,setSocialLinks] = useState({});
+
+    useEffect(()=>{
+        function loadLinks(){
+            const linksRef = collection(db,"links");
+            const queryRef = query(linksRef,orderBy("created","asc"));
+
+            getDocs(queryRef).then((snapshot)=>{
+                let lista = [];
+                snapshot.forEach((doc)=>{
+                    lista.push({
+                        id:doc.id,
+                        name: doc.data().name,
+                        url: doc.data().url,
+                        bg: doc.data().bg,
+                        color: doc.data().color,
+                    });
+                });
+                setLinks(lista);
+                })
+            }
+            loadLinks();           
+        },[]);
+    
+        useEffect(()=>{
+            function loadSocialLinks(){
+                const docRef = doc(db,"social","link");
+                getDoc(docRef).then((snapshot)=>{
+                    if(snapshot.data()!== undefined){
+                        setSocialLinks({
+                            facebook: snapshot.data().facebook,
+                            instagram: snapshot.data().instagram,
+                            linkedIn: snapshot.data().linkedIn,
+                            github: snapshot.data().github,
+                        })
+                    }
+                })
+            }
+            loadSocialLinks();
+        },[]);
+
     return(
         <div className="home-container">
             <h1>Nome</h1>
@@ -10,38 +64,37 @@ export default function Home(){
 
 
             <main className="links">
-                <section className="link-area">
-                    <a href="#">
-                        <p className="link-text">Canal no Youtube</p>
-                    </a>
-                </section>
+                {links.map((item,index)=>(
+                     <section 
+                     key={item.id}
+                     className="link-area"
+                     style={{backgroundColor: item.bg}}>
+                     
+                     <a href={item.url} target="blank">
+                         <p className="link-text" style={{color:item.color}}>{item.name}</p>
+                     </a>
+                 </section>
+ 
+                ))}
 
-                <section className="link-area">
-                    <a href="#">
-                        <p className="link-text">Canal no Youtube</p>
-                    </a>
-                </section>
-
-                <section className="link-area">
-                    <a href="#">
-                        <p className="link-text">Canal no Youtube</p>
-                    </a>
-                </section>
-
-                <footer>
-                    <Social url="https://facebook.com">
+                {links.length!== 0 && Object.keys(socialLinks).length>0 && (
+                    <footer>
+                    <Social url={socialLinks.facebook}>
                         <FaFacebook size={35} color="#FFF"/>
                     </Social>
 
-                    <Social url="https://youtube.com">
+                    <Social url={socialLinks.instagram}>
                         <FaYoutube size={35} color="#FFF"/>
                     </Social>
 
-                    <Social url="https://instagram.com">
+                    <Social url={socialLinks.linkedIn}>
                         <FaInstagram size={35} color="#FFF"/>
                     </Social>
 
                 </footer>
+                )}
+                 
+               
             </main>
         </div>
     )
